5a5cf9bac24121ba560b0f01525901213b94d5d4
Manyati-2020 <manymunya@gmail.com>
2021-03-31 12:02:49 +0200

    Manyati M Class Example 2021

---
+++ src/za/ac/cput/Addition.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+/**
+ * Author Munyaradzi Manyati
+ * Student Number 215211855
+ */
+
+public class Addition {
+
+    public int integerSum(int a , int b) {
+        return a + b;
+    }
+}
